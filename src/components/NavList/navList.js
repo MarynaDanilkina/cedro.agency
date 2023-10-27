b0bd5210ae654d5bd8831a161202e069d45c116c
MarynaDanilkina <Danilkinamari99@gmail.com>
2023-10-27 09:35:10 +0300

    fix: fix navList

--- src/components/NavList/navList.js
+++ src/components/NavList/navList.js
@@ -1,10 +1,24 @@
+import { useMemo } from "react";
 import Droplist from "../Droplist/droplist";
 import "./navList.css";
+import cn from "classnames";
 
 const NavList = ({ data, activeData, getActiveData }) => {
+
+  const classes = useMemo(
+    () =>
+      cn(
+        {
+          "middle": activeData.length === 0,
+          "small": activeData.length > 0
+        }
+      ),
+    [activeData]
+  )
+
   return (
     <div className="navList__wrapper">
-      <div className="navList__container">
+      <div className={`${classes} navList__container`}>
         {data.data.map((el) => (
           <Droplist key={el} text={el} activeData={activeData} getActiveData={getActiveData} />
         ))}
